32bef3c87ffcf70f1c269b89642eaddc894b5f41
vkalaparthy <kalaparthy.vani@gmail.com>
2020-10-23 14:29:26 -0400

    added service-worker with install & activate

---
+++ public/service-worker.js
@@ -0,0 +1,48 @@
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/manifest.webmanifest",
+  "/styles.css",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+  "/index.js"
+];
+
+// install
+self.addEventListener("install", function (evt) {
+  // pre cache image data
+  console.log("********  In install");
+    
+  // pre cache all static assets
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+
+  // tell the browser to activate this service worker immediately once it
+  // has finished installing
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function(evt) {
+  console.log("****  In activate ****");
+  evt.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+
